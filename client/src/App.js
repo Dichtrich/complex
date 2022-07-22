37505775a363459b9c1a80eb4f0b51dc39d2acff
nsalfos <107048281+nsalfos@users.noreply.github.com>
2022-07-22 16:26:20 +0200

    testing AWS deploy

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            IS THIS THE MOTHERFUCKING GREETING?
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
